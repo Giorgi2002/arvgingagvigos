24e4225efc74758ed67fb9889a5135c945eab1ac
alexa1121 <dzadzamialeqso27@gmail.com>
2022-12-01 19:14:45 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 "use strict";
 const box = document.querySelectorAll(".btn");
 const hide = document.querySelector(".btn-alive");
+let score = 0;
+const txutmet = (document.querySelector(".txutmet").textContent = "/15");
 
 for (let i = 0; i < box.length; i++) {
   if (box[i].textContent == "მკვდარი") {
     box[i].classList.remove("btn-alive");
     box[i].classList.add("btn-dead");
+    score++;
+    document.querySelector(".zero").textContent = score + txutmet;
   }
 }
