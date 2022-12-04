9727bdbc576efad16f713b6137fd545838899b2a
Kama <giorgi2002gio@gmail.com>
2022-12-04 10:34:28 +0400

    sanamgaixsneba

--- script.js
+++ script.js
@@ -1,14 +1,43 @@
-"use strict";
-const box = document.querySelectorAll(".btn");
-const hide = document.querySelector(".btn-alive");
-let score = 0;
-const txutmet = (document.querySelector(".txutmet").textContent = "/15");
+﻿(function () {
+  const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
 
-for (let i = 0; i < box.length; i++) {
-  if (box[i].textContent == "მკვდარი") {
-    box[i].classList.remove("btn-alive");
-    box[i].classList.add("btn-dead");
-    score++;
-    document.querySelector(".zero").textContent = score + txutmet;
+  //I'm adding this section so I don't have to keep updating this pen every year :-)
+  //remove this if you don't need it
+  let today = new Date(),
+      dd = String(today.getDate()).padStart(2, "0"),
+      mm = String(today.getMonth() + 1).padStart(2, "0"),
+      yyyy = today.getFullYear(),
+      nextYear = yyyy + 1,
+      dayMonth = "12/31/",
+      gaxsna = dayMonth + yyyy;
+  
+  today = mm + "/" + dd + "/" + yyyy;
+    if (today > gaxsna) {
+        gaxsna = dayMonth + nextYear;
   }
-}
+  //end
+  
+    const countDown = new Date(gaxsna).getTime(),
+      x = setInterval(function() {    
+
+        const now = new Date().getTime(),
+              distance = countDown - now;
+
+        document.getElementById("days").innerText = Math.floor(distance / (day)),
+          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+        //do something later when date is reached
+        if (distance < 0) {
+          document.getElementById("headline").innerText = "გავიხსენით!";
+          document.getElementById("countdown").style.display = "none";
+          document.getElementById("content").style.display = "block";
+          clearInterval(x);
+        }
+        //seconds
+      }, 0)
+  }());
